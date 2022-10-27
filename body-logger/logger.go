69c03e9d3df7938b7bedec02ed501cf01c796b9a
armourstill <armourstill@hotmail.com>
2022-10-27 19:56:46 +0800

    Add common files and two middlewares (#2)

--- body-logger/logger.go
+++ body-logger/logger.go
@@ -3,103 +3,109 @@ package bodylogger
 import (
 	"bytes"
 	"fmt"
-	"io"
 	"io/ioutil"
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 type Config struct {
-	gin.LoggerConfig
+	GinConfig *gin.LoggerConfig
 
 	BodyLogFormatter Formatter
-	Request          bool
-	BeforeRequest    bool
+	// Log the request body or not
+	WithBody bool
+	// Only body of these methods can be logged, defaults to [POST, PATCH, PUT] when value is nil
+	Methods map[string]struct{}
+	// If true, the log will output after all other midllwares and the final func
+	AfterRequest bool
 }
 
-type Formatter func(params FormatterParams) string
+type Formatter func(params *FormatterParams) string
 
 type FormatterParams struct {
-	gin.LogFormatterParams
+	ginParam *gin.LogFormatterParams
 
 	RequestBody []byte
 }
 
-var defaultBodyLogFormatter = func(param FormatterParams) string {
+var defaultBodyLogFormatter = func(param *FormatterParams) string {
 	var statusColor, methodColor, resetColor string
-	if param.IsOutputColor() {
-		statusColor = param.StatusCodeColor()
-		methodColor = param.MethodColor()
-		resetColor = param.ResetColor()
+	if param.ginParam.IsOutputColor() {
+		statusColor = param.ginParam.StatusCodeColor()
+		methodColor = param.ginParam.MethodColor()
+		resetColor = param.ginParam.ResetColor()
 	}
 
-	if param.Latency > time.Minute {
-		param.Latency = param.Latency.Truncate(time.Second)
+	if param.ginParam.Latency > time.Minute {
+		param.ginParam.Latency = param.ginParam.Latency.Truncate(time.Second)
 	}
 	baseLog := fmt.Sprintf("[GIN] %v |%s %3d %s| %13v | %15s |%s %-7s %s %#v\n%s",
-		param.TimeStamp.Format("2006/01/02 - 15:04:05"),
-		statusColor, param.StatusCode, resetColor,
-		param.Latency,
-		param.ClientIP,
-		methodColor, param.Method, resetColor,
-		param.Path,
-		param.ErrorMessage,
+		param.ginParam.TimeStamp.Format("2006/01/02 - 15:04:05"),
+		statusColor, param.ginParam.StatusCode, resetColor,
+		param.ginParam.Latency,
+		param.ginParam.ClientIP,
+		methodColor, param.ginParam.Method, resetColor, param.ginParam.Path,
+		param.ginParam.ErrorMessage,
 	)
 	if len(param.RequestBody) > 0 {
-		return baseLog + "\n" + string(param.RequestBody)
+		return baseLog + string(param.RequestBody)
 	}
 	return baseLog
 }
 
-var beforeRequestFormatter = func(param FormatterParams) string {
-	var statusColor, methodColor, resetColor string
-	if param.IsOutputColor() {
-		statusColor = param.StatusCodeColor()
-		methodColor = param.MethodColor()
-		resetColor = param.ResetColor()
+var beforeRequestFormatter = func(param *FormatterParams) string {
+	var methodColor, resetColor string
+	if param.ginParam.IsOutputColor() {
+		methodColor = param.ginParam.MethodColor()
+		resetColor = param.ginParam.ResetColor()
 	}
 
-	baseLog := fmt.Sprintf("[GIN] %v |%s %3d %s| %15s |%s %-7s %s\n%s",
-		param.TimeStamp.Format("2006/01/02 - 15:04:05"),
-		statusColor, param.StatusCode, resetColor,
-		param.ClientIP,
-		methodColor, param.Method, resetColor,
-		param.Path,
+	baseLog := fmt.Sprintf("[GIN] %v |%s %-7s %s %#v\n",
+		param.ginParam.TimeStamp.Format("2006/01/02 - 15:04:05"),
+		methodColor, param.ginParam.Method, resetColor,
+		param.ginParam.Path,
 	)
 	if len(param.RequestBody) > 0 {
-		return baseLog + "\n" + string(param.RequestBody)
+		return baseLog + string(param.RequestBody)
 	}
 	return baseLog
 }
 
-func BodyLogger(request, beforeRequest bool) gin.HandlerFunc {
-	return WithConfig(Config{
-		LoggerConfig: gin.LoggerConfig{}, Request: request, BeforeRequest: beforeRequest,
+func BodyLogger(withBody, AfterRequest bool, methods ...string) gin.HandlerFunc {
+	if methods == nil {
+		return WithConfig(&Config{
+			GinConfig: &gin.LoggerConfig{}, WithBody: withBody, AfterRequest: AfterRequest,
+		})
+	}
+	m := make(map[string]struct{})
+	for _, method := range methods {
+		m[method] = struct{}{}
+	}
+	return WithConfig(&Config{
+		GinConfig: &gin.LoggerConfig{}, WithBody: withBody, Methods: m, AfterRequest: AfterRequest,
 	})
 }
 
-func LoggerWithWriter(request, beforeRequest bool, out io.Writer, notlogged ...string) gin.HandlerFunc {
-	return WithConfig(Config{
-		LoggerConfig: gin.LoggerConfig{Output: out, SkipPaths: notlogged}, Request: request, BeforeRequest: beforeRequest,
-	})
-}
-
-func WithConfig(conf Config) gin.HandlerFunc {
+func WithConfig(conf *Config) gin.HandlerFunc {
 	formatter := conf.BodyLogFormatter
+	if !conf.AfterRequest {
+		formatter = beforeRequestFormatter
+	}
 	if formatter == nil {
 		formatter = defaultBodyLogFormatter
 	}
-	if conf.BeforeRequest {
-		formatter = beforeRequestFormatter
+	if conf.Methods == nil {
+		conf.Methods = map[string]struct{}{http.MethodPost: {}, http.MethodPatch: {}, http.MethodPut: {}}
 	}
 
-	out := conf.Output
+	out := conf.GinConfig.Output
 	if out == nil {
 		out = gin.DefaultWriter
 	}
 
-	notlogged := conf.SkipPaths
+	notlogged := conf.GinConfig.SkipPaths
 
 	var skip map[string]struct{}
 
@@ -113,13 +119,12 @@ func WithConfig(conf Config) gin.HandlerFunc {
 
 	return func(c *gin.Context) {
 		path := c.Request.URL.Path
-
-		if _, ok := skip[path]; !ok {
+		if _, ok := skip[path]; ok {
 			return
 		}
 
-		param := FormatterParams{
-			LogFormatterParams: gin.LogFormatterParams{
+		param := &FormatterParams{
+			ginParam: &gin.LogFormatterParams{
 				Request:  c.Request,
 				Keys:     c.Keys,
 				ClientIP: c.ClientIP(),
@@ -130,32 +135,30 @@ func WithConfig(conf Config) gin.HandlerFunc {
 		start := time.Now()
 		raw := c.Request.URL.RawQuery
 
-		if conf.Request {
+		if _, ok := conf.Methods[c.Request.Method]; conf.WithBody && ok {
 			body, _ := ioutil.ReadAll(c.Request.Body)
 			param.RequestBody = body
 			// Because the HTTP request body can be read only once, we write back the request body with NopCloser
 			c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 		}
 
-		if !conf.BeforeRequest {
+		param.ginParam.TimeStamp = start
+		if conf.AfterRequest {
 			// Process request
 			c.Next()
 			// Stop timer
-			param.TimeStamp = time.Now()
-			param.Latency = param.TimeStamp.Sub(start)
-
-			param.StatusCode = c.Writer.Status()
-			param.ErrorMessage = c.Errors.ByType(gin.ErrorTypePrivate).String()
-			param.BodySize = c.Writer.Size()
-		} else {
-			param.TimeStamp = start
+			param.ginParam.TimeStamp = time.Now()
+			param.ginParam.Latency = param.ginParam.TimeStamp.Sub(start)
+			param.ginParam.StatusCode = c.Writer.Status()
+			param.ginParam.ErrorMessage = c.Errors.ByType(gin.ErrorTypePrivate).String()
+			param.ginParam.BodySize = c.Writer.Size()
 		}
 
 		if raw != "" {
 			path = path + "?" + raw
 		}
 
-		param.Path = path
+		param.ginParam.Path = path
 
 		fmt.Fprint(out, formatter(param))
 	}
